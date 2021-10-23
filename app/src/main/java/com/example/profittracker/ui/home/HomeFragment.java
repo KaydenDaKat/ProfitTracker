a454cb749ef8d5f0c8c65c7a3c364bad7523be07
KaydenDaKat <gippy713@gmail.com>
2021-10-23 01:10:43 -0700

    Added the profit goal progress circle and other changes

--- app/src/main/java/com/example/profittracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/profittracker/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.profittracker.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,16 +22,17 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        TextView textViewProfitRatio = root.findViewById(R.id.textView_profitRatio_homeFragment);
+        ProgressBar progressBarGoalProgress = root.findViewById(R.id.progressBar_goalProgress_homeFragment);
+
+        textViewProfitRatio.setText("1000/6000");
+        progressBarGoalProgress.setProgress(60);
+        root.getRootView().setBackgroundColor(Color.parseColor("#00BCD4")); //custom blue
+
         return root;
     }
+
 }
